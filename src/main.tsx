02c199d48d4d9e62edd1676c5accc9169be185d4
Colin Brooks <colinrobertbrooks@gmail.com>
2024-01-25 08:52:07 -0500

    refactor: router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import "./main.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { EventsProvider } from "./contexts";
-import router from "./router";
+import { CalendarPage, AddEventPage } from "./pages";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <EventsProvider>
-      <RouterProvider router={router} />
-    </EventsProvider>
+    <BrowserRouter>
+      <EventsProvider>
+        <Routes>
+          <Route path="/" element={<CalendarPage />}>
+            <Route path="/add-event" element={<AddEventPage />} />
+          </Route>
+        </Routes>
+      </EventsProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
