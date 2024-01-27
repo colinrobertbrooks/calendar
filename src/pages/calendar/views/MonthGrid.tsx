e60e97d6a570a16060151afb0a6ded64e0fb093b
Colin Brooks <colinrobertbrooks@gmail.com>
2024-01-27 10:32:14 -0500

    feat: highlight today's date

--- src/pages/calendar/views/MonthGrid.tsx
+++ src/pages/calendar/views/MonthGrid.tsx
@@ -1,6 +1,6 @@
 import { MONTH_VIEW } from "../../../constants";
 import { DayOfWeek } from "../../../types";
-import { getEnumKeys, makeShort } from "../../../utils";
+import { getEnumKeys, getToday, isSameDate, makeShort } from "../../../utils";
 import AddEventButton from "../AddEventButton";
 import Events from "../Events";
 
@@ -9,40 +9,53 @@ type Props = {
   month: number;
 };
 
-export const MonthGrid = ({ data, month }: Props) => (
-  <>
-    <div className="grid grid-cols-7 gap-2">
-      {getEnumKeys(DayOfWeek).map((dayOfWeek) => (
-        <div key={dayOfWeek} className="text-right pr-2">
-          {makeShort(dayOfWeek)}
+export const MonthGrid = ({ data, month }: Props) => {
+  const today = getToday();
+
+  return (
+    <>
+      <div className="grid grid-cols-7 gap-2">
+        {getEnumKeys(DayOfWeek).map((dayOfWeek) => (
+          <div key={dayOfWeek} className="text-right pr-2">
+            {makeShort(dayOfWeek)}
+          </div>
+        ))}
+      </div>
+      {data.map((week, idx) => (
+        <div key={idx} className="grid grid-cols-7 gap-2 mb-2 last:mb-0">
+          {week.map((date) => {
+            const isMonth = date.getMonth() === month;
+            const isToday = isSameDate(date, today);
+
+            return (
+              <div
+                key={date.toUTCString()}
+                className={`group aspect-square border ${
+                  isMonth
+                    ? "border-gray-400 text-gray-800"
+                    : "border-gray-200 text-gray-200"
+                } p-2`}
+              >
+                <div className="text-right mb-1">
+                  <span
+                    className={
+                      isMonth && isToday ? "p-1 rounded-full bg-orange-100" : ""
+                    }
+                  >
+                    {date.getDate()}
+                  </span>
+                </div>
+                {isMonth && (
+                  <>
+                    <Events date={date} view={MONTH_VIEW} />
+                    <AddEventButton date={date} />
+                  </>
+                )}
+              </div>
+            );
+          })}
         </div>
       ))}
-    </div>
-    {data.map((week, idx) => (
-      <div key={idx} className="grid grid-cols-7 gap-2 mb-2 last:mb-0">
-        {week.map((date) => {
-          const isMonth = date.getMonth() === month;
-
-          return (
-            <div
-              key={date.toUTCString()}
-              className={`group aspect-square border ${
-                isMonth
-                  ? "border-gray-400 text-gray-800"
-                  : "border-gray-200 text-gray-200"
-              } p-2`}
-            >
-              <div className="text-right">{date.getDate()}</div>
-              {isMonth && (
-                <>
-                  <Events date={date} view={MONTH_VIEW} />
-                  <AddEventButton date={date} />
-                </>
-              )}
-            </div>
-          );
-        })}
-      </div>
-    ))}
-  </>
-);
+    </>
+  );
+};
--- src/pages/calendar/views/WeekGrid.tsx
+++ src/pages/calendar/views/WeekGrid.tsx
@@ -1,5 +1,5 @@
 import { WEEK_VIEW } from "../../../constants";
-import { getDayOfWeekName } from "../../../utils";
+import { getDayOfWeekName, getToday, isSameDate } from "../../../utils";
 import AddEventButton from "../AddEventButton";
 import Events from "../Events";
 
@@ -8,35 +8,47 @@ type Props = {
   month: number;
 };
 
-export const WeekGrid = ({ data, month }: Props) => (
-  <div className="flex-1 grid grid-cols-7 gap-2">
-    {data.map((date) => {
-      const isMonth = date.getMonth() === month;
+export const WeekGrid = ({ data, month }: Props) => {
+  const today = getToday();
 
-      return (
-        <div
-          key={date.toUTCString()}
-          className={`flex flex-col group ${
-            isMonth ? "text-gray-800" : "text-gray-200"
-          }`}
-        >
-          <div className="text-center">
-            {getDayOfWeekName(date.getDay(), true)} {date.getDate()}
-          </div>
+  return (
+    <div className="flex-1 grid grid-cols-7 gap-2">
+      {data.map((date) => {
+        const isMonth = date.getMonth() === month;
+        const isToday = isSameDate(date, today);
+
+        return (
           <div
-            className={`flex-1 border ${
-              isMonth ? "border-gray-400" : "border-gray-200"
-            } p-2`}
+            key={date.toUTCString()}
+            className={`flex flex-col group ${
+              isMonth ? "text-gray-800" : "text-gray-200"
+            }`}
           >
-            {isMonth && (
-              <>
-                <Events date={date} view={WEEK_VIEW} />
-                <AddEventButton date={date} />
-              </>
-            )}
+            <div className="text-center mb-1">
+              {getDayOfWeekName(date.getDay(), true)}{" "}
+              <span
+                className={
+                  isMonth && isToday ? "p-1 rounded-full bg-orange-100 " : ""
+                }
+              >
+                {date.getDate()}
+              </span>
+            </div>
+            <div
+              className={`flex-1 border ${
+                isMonth ? "border-gray-400" : "border-gray-200"
+              } p-2`}
+            >
+              {isMonth && (
+                <>
+                  <Events date={date} view={WEEK_VIEW} />
+                  <AddEventButton date={date} />
+                </>
+              )}
+            </div>
           </div>
-        </div>
-      );
-    })}
-  </div>
-);
+        );
+      })}
+    </div>
+  );
+};
