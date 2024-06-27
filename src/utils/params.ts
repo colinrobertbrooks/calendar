b1daa63bd472ee0a63f7816449ee16176a96a8d6
Colin Brooks <colinrobertbrooks@gmail.com>
2024-06-27 13:31:33 -0400

    fix: fromDateParam

--- src/utils/params.ts
+++ src/utils/params.ts
@@ -2,7 +2,7 @@ export const toDateParam = (date: Date) =>
   new Date(date).toISOString().slice(0, 10); // YYYY-MM-DD
 
 export const fromDateParam = (dateParam: string): Date => {
-  const date = new Date(dateParam);
+  const date = new Date(`${dateParam}T00:00:00`);
   date.setHours(0, 0, 0, 0);
   return date;
 };
